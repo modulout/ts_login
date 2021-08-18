24a9ad9977a5ad2987c2ba39e51a8e08da142fe2
Modulout <info@modulout.com>
2021-08-18 09:33:18 +0200

    sanitize data

--- php/TSL_Admin.php
+++ php/TSL_Admin.php
@@ -14,10 +14,43 @@ class TSL_Admin {
     }
 
     /* Save all values */
-    public function tsl_save_config() { 
+    public function tsl_save_config() {
         $data = $_POST;
-        foreach($data as $value => $item) {
-            update_option($value, sanitize_text_field($item));
+
+        $all_data = [
+            "tsl_form_template"        => sanitize_text_field($data['tsl_form_template']),
+            "tsl_form_image"           => esc_url($data['tsl_form_image']),
+            "tsl_logout_option"        => sanitize_text_field($data['tsl_logout_option']),
+            "tsl_logout_option"        => sanitize_text_field($data['tsl_logout_option']),
+            "tsl_bbgc"                 => sanitize_hex_color($data['tsl_bbgc']),
+            "tsl_btc"                  => sanitize_hex_color($data['tsl_btc']),
+            "tsl_bbc"                  => sanitize_hex_color($data['tsl_bbc']),
+            "tsl_bhbgc"                => sanitize_hex_color($data['tsl_bhbgc']),
+            "tsl_bhtc"                 => sanitize_hex_color($data['tsl_bhtc']),
+            "tsl_bhbc"                 => sanitize_hex_color($data['tsl_bhbc']),
+            "tsl_ddbgc"                => sanitize_hex_color($data['tsl_ddbgc']),
+            "tsl_ddtc"                 => sanitize_hex_color($data['tsl_ddtc']),
+            "tsl_ddhtc"                => sanitize_hex_color($data['tsl_ddhtc']),
+            "tsl_hbgc"                 => sanitize_hex_color($data['tsl_hbgc']),
+            "tsl_htc"                  => sanitize_hex_color($data['tsl_htc']),
+            "tsl_cbgc"                 => sanitize_hex_color($data['tsl_cbgc']),
+            "tsl_ctc"                  => sanitize_hex_color($data['tsl_ctc']),
+            "tsl_cibgc"                => sanitize_hex_color($data['tsl_cibgc']),
+            "tsl_citc"                 => sanitize_hex_color($data['tsl_citc']),
+            "tsl_cibc"                 => sanitize_hex_color($data['tsl_cibc']),
+            "tsl_sbbgc"                => sanitize_hex_color($data['tsl_sbbgc']),
+            "tsl_sbtc"                 => sanitize_hex_color($data['tsl_sbtc']),
+            "tsl_sbbc"                 => sanitize_hex_color($data['tsl_sbbc']),
+            "tsl_sbhbgc"               => sanitize_hex_color($data['tsl_sbhbgc']),
+            "tsl_sbhtc"                => sanitize_hex_color($data['tsl_sbhtc']),
+            "tsl_sbhbc"                => sanitize_hex_color($data['tsl_sbhbc']),
+            "tsl_recaptcha_enable"     => sanitize_text_field($data['tsl_recaptcha_enable']),
+            "tsl_recaptcha_site_key"   => sanitize_text_field($data['tsl_recaptcha_site_key']),
+            "tsl_recaptcha_secret_key" => sanitize_text_field($data['tsl_recaptcha_secret_key']),
+            "tsl_recaptcha_badge"      => sanitize_text_field($data['tsl_recaptcha_badge']),
+        ];
+        foreach($all_data as $value => $item) {
+            update_option($value, $item);
         }
 
         //Save all colors - only 1 request on frontend to get all colors
